867e19b027246bc72af2bd613345a380934b219c
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-03-17 22:52:44 +0530

    Update traversal.cpp

--- Tree/traversal.cpp
+++ Tree/traversal.cpp
@@ -2,7 +2,26 @@
 
 void(vector<int>v, Node* root){
   if(root == null)return;
+
+  solve(v,root->left);
+  v.push_back(root);
+  solve(v,root->right);
+}
+
+// Preorder
+void(vector<int>v, Node* root){
+  if(root == null)return;
   v.push_back(root);
   solve(v,root->left);
   solve(v,root->right);
+}
+
+
+// Postorder
+void(vector<int>v, Node* root){
+  if(root == null)return;
+  
+  solve(v,root->left);
+  solve(v,root->right);
+  v.push_back(root);
 }
