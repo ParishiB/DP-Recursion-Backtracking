7ef6d13be12bd4d5844c7473e1ff1389c9dfc8b5
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-03-17 22:45:47 +0530

    Update bfs.cpp

--- Graph/bfs.cpp
+++ Graph/bfs.cpp
@@ -1,1 +1,22 @@
+vector<int>bfs(int V , vector<int>adj[]){
+	vector<int>vis(V,0);
+	vis[0] = 1;
+	queue<int>q;
+	q.push(0);
+	vetor<int>ans;
 
+	while(!q.empty()){
+		int temp = q.front();
+		q.pop();
+		v.push_back(temp);
+
+		for(auto it : adj[temp]){
+			if(vis[it] == 0){
+				vis[it] = 1;
+				q.push(it);
+			}
+		}
+	}
+
+	return v;
+}
