a8e62ea4796a6d5874acd4e6e44a381894ce1b19
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-03-17 22:49:16 +0530

    Update dfs.cpp

--- Graph/dfs.cpp
+++ Graph/dfs.cpp
@@ -1,1 +1,20 @@
+void solve(int it , int V, vector<int>adl[], vector<int>vis , vector<int>ans){
 
+	vis[it] = 1;
+	ans.push_back(it);
+
+	for(auto i: vis[it]){
+		if(vis[i] == 0){
+           solve(i,V,adj,vis,ans);
+		}
+      
+	}
+}
+
+
+vector<int>bfs(int V , vector<int>adj[]){
+	vector<int>vis(V,0);
+	vector<int>ans;
+	solve(0,V,adj,vis,ans);
+	return ans;
+}
